e50d0f01a61224ebcb0a3cfa1454f3ee9522c0d5
Sunil Sahu <sunil.rock.9026040@gmail.com>
2024-07-17 17:23:30 +0530

    changes-modified 3

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -30,7 +30,7 @@ export const MuiTblOptions = () => {
 };
 
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL ||
+  // process.env.NEXT_PUBLIC_BASE_URL ||
   "http://ec2-3-83-254-115.compute-1.amazonaws.com:8020/api/v1";
 
 //? SET To LocalStorage
